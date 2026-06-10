7dec34261ead64f90076afcd03f1bc12d91853cd
Hadi Alsibassi <hadisibassi@gmail.com>
2026-06-10 16:56:58 -0400

    adding lazygit to remote

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -3,6 +3,15 @@ return {
   version = "*",
   keys = {
     { "<C-t>", "<cmd>ToggleTerm direction=vertical<cr>", desc = "Toggle Terminal (vertical)" },
+    {
+      "<leader>gg",
+      function()
+        local Terminal = require("toggleterm.terminal").Terminal
+        local lazygit = Terminal:new({ cmd = "lazygit", hidden = true, direction = "float" })
+        lazygit:toggle()
+      end,
+      desc = "Lazygit",
+    },
   },
   config = function()
     require("toggleterm").setup({
